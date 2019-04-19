ef498ea91e09b018b6ef4290bb7c2d55a6949376
poppo0210 <49470366+poppo0210@users.noreply.github.com>
2019-04-19 12:03:02 +0900

    Update app.py

--- app.py
+++ app.py
@@ -17,11 +17,11 @@ app = Flask(__name__)
 
 
 #環境変数からLINE Access Tokenを設定
-LINE_bot_CHANNEL_ACCESS_TOKEN = os.environ["LINE_BOT_CHANNEL_TOKEN"]
+LINE_BOT_CHANNEL_ACCESS_TOKEN = os.environ["LINE_BOT_CHANNEL_TOKEN"]
 #環境変数からLINE Channel Secretを設定
-LINE_bot_CHANNEL_SECRET = os.environ["LINE_BOT_CHANNEL_SECRET"]
+LINE_BOT_CHANNEL_SECRET = os.environ["LINE_BOT_CHANNEL_SECRET"]
 
-line_bot_api = LineBotApi(LINE__BOT_CHANNEL_ACCESS_TOKEN)
+line_BOT_api = LineBotApi(LINE__BOT_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_BOT_CHANNEL_SECRET)
 
 @app.route("/callback", methods=['POST'])
