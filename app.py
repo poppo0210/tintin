49607e0555cd3fd76f5d2285461b82a155f2cf9a
poppo0210 <49470366+poppo0210@users.noreply.github.com>
2019-06-25 15:32:40 +0900

    Update app.py

--- app.py
+++ app.py
@@ -9,15 +9,19 @@ from linebot.exceptions import (
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
-import os
+
 
 app = Flask(__name__)
 
 #環境変数取得
 talk = {
-    "こんにちは":"こんにちは",
+    "おっぱい大きいね":"お客さんもね！",
+    "どんな人がタイプ？":"風俗利用しない人ですかねー",
+    "俺のこと好き？":"あー、うん（時計見ながら）",
+    "ちょっとだけ入れていい？":"今度来てくれたらね(^.^)",
+    "相性いいよね":"ほんとだね～",
+    "今度食事行こうよ":"ハイ、お店予約時に言って頂ければ",
 }
-
 
 line_bot_api = LineBotApi('XXH3eq5CN3s7L0aoZm9ah70fwdg8uLncGNd7PuN9JSzzDMC7XWayg2vXSAZLno8nMlOCG00KmbJqt822AeYowRS1IrkoiV/k9aM1sIVfXTFIssq52IVyWpoKGeM/vrc14RtA6Dp8RGm38yJ0WizCEQdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('93f48b7595a4d0035a99c38ecb2d1a02')
